6dc4db233cd4e88b230693386409ec6e4e39f96c
Seth Rhodes <seth@thinkpixbit.com>
2014-10-15 20:44:28 -0400

    working on redone pokemon game, going to rewrite

--- rev_pokemon_game.rb
+++ rev_pokemon_game.rb
@@ -1,5 +1,5 @@
 def createPokemon(name,type,level)
-  pokemon = {:name => name, :type => type, :level => level}
+  pokemon = {:name => name, :type => type, :level => level, :health => 100}
   return pokemon
 end
 
@@ -9,11 +9,11 @@ bulbasaur = createPokemon("Bulbasaur","grass",5)
 pokemon = [squirtle,bulbasaur]
 pokemon.each {|k| puts k }
 
-attack_array = [['bite',30],['tackle',15],['quick attack', 10],['special attack', 45]]
+attacks = [['bite',30],['tackle',15],['quick attack', 10],['special attack', 45]]
 def addAttacks(array, pokemon)
 
 end
 
 def battle(pokemon_one, pokemon_two)
-
 end
+puts "alive" if squirtle[:health] > 0
